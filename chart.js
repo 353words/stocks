30378e7a4c35cb3e666e9e7c3c7bfb8d6951b714
Miki Tebeka <miki@353solutions.com>
2021-12-30 19:49:36 +0200

    div by ID

--- chart.js
+++ chart.js
@@ -2,8 +2,7 @@ async function updateChart() {
     let symbol = document.getElementById('symbol').value;
     let resp = await fetch('/data?symbol=' + symbol);
     let data = await resp.json(); 
-    let chart = document.getElementById('chart');
-    Plotly.newPlot(chart, data.data, data.layout);
+    Plotly.newPlot('chart', data.data, data.layout);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
